3435e17c939f490e7e9735b9e23daf076254ee22
sphoid <sphoid+github@gmail.com>
2025-01-26 12:35:47 -0800

    WIP

---
+++ src/config.rs
@@ -0,0 +1,37 @@
+use std::error::Error;
+use std::path::Path;
+use std::io::BufReader;
+use serde::{Deserialize, Serialize};
+
+const CONFIG_FILE: &str = "config.json";
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum PersistenceMode {
+	JSON,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Config {
+	persistence_mode: PersistenceMode,
+}
+
+impl Config {
+	pub fn default() -> Self {
+		Self {
+			persistence_mode: PersistenceMode::JSON,
+		}
+	}
+}
+
+pub fn load_config() -> Result<Config, Box<dyn Error>> {
+	let config_path = Path::new(CONFIG_FILE);
+	if !config_path.exists() {
+		return Ok(Config::default());
+	}
+
+	let file = std::fs::File::open(config_path)?;
+	let reader = BufReader::new(file);
+	let config: Config = serde_json::from_reader(reader)?;
+
+	Ok(config)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 pub mod project;
+pub mod config;
+// pub use crate::project::ProjectTaskType;
+// pub use crate::project::ProjectTaskStatus;
+// pub use crate::project::ProjectTask;
+// pub use crate::project::Project;
+// pub use crate::project::ProjectData;
 
-pub use crate::project::ProjectTaskType;
-pub use crate::project::ProjectTaskStatus;
-pub use crate::project::ProjectTask;
-pub use crate::project::Project;
-pub use crate::project::ProjectData;
-
-pub use crate::project::load_data;
-pub use crate::project::write_data;
+// pub use crate::project::load_data;
+// pub use crate::project::write_data;
